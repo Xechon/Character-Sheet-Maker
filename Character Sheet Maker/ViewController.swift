a74f9d2ca407466ac42a33dbb79ff800c3cbcd31
Xechon <guess96@gmail.com>
2016-11-17 21:13:50 -0600

    Initial commit
    Nothing here yet...

---
+++ Character Sheet Maker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Character Sheet Maker
+//
+//  Created by Programmer on 11/17/16.
+//  Copyright © 2016 Joanie's Fish Daycare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
